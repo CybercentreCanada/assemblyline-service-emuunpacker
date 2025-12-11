05c7a9820c2900ad14485e12ba01a3b0f416f3ec
cccs-jaw <244584135+cccs-jaw@users.noreply.github.com>
2025-12-11 19:23:13 +0000

    Updated to respect timeout configuration.

--- emuunpacker/emuunpacker.py
+++ emuunpacker/emuunpacker.py
@@ -13,15 +13,31 @@ from .unpacker import UnpackError, unpack
 class EmuUnpacker(ServiceBase):
     """Assemblyline service unpacks binaries by leveraging the unicorn emulation library."""
 
+    # Aim to complete processing within a safe margin of seconds before configured timeout.
+    TIMEOUT_SAFETY_MARGIN = 5
+
+    # Time allocated after emulation to produce a dump
+    TIME_ALLOCATION_DUMP = 5
+
+    # To be useful, at least 2 seconds should be allocated for emulation runtime
+    TIME_ALLOCATION_EMULATION = 2
+
     def execute(self, request: ServiceRequest) -> None:
         """Run the service. Returns the result or None if no result was produced."""
         result = Result()
         request.result = result
 
+        emulation_runtime = self.service_attributes.timeout - EmuUnpacker.SERVICE_TIMEOUT_SAFETY_MARGIN
+        emulation_runtime -= EmuUnpacker.TIME_ALLOCATION_DUMP
+
+        if emulation_runtime < EmuUnpacker.TIME_ALLOCATION_EMULATION:
+            self.log.error("Could not allocate sufficient time for emulation runtime. Increase service timeout config.")
+            return
+
         results_dir = tempfile.mkdtemp(dir=self.working_directory)
 
         try:
-            unpacked_result = unpack(request.file_path, results_dir, timeout_seconds=60 * 5)
+            unpacked_result = unpack(request.file_path, results_dir, timeout_seconds=emulation_runtime)
         except UnpackError as e:
             self.log.error(f"EmuUnpacker extractor failed:\n{e}")
             return
